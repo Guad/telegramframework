dde801cb0585d3e352ce231036ef194ea3239cf0
Guad <rockeurp@gmail.com>
2017-10-06 17:18:42 +0200

    added fortune

---
+++ Features/Fortune.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using File = System.IO.File;
+
+namespace CoreDumpedTelegramBot.Features
+{
+    public class Fortune : IBotPlugin
+    {
+        private string[] _fortune;
+        private Random _r = new Random();
+
+        public void Hook(TelegramBotClient bot){}
+        public void Update(){}
+        public void Stop() {}
+
+
+        public void Start()
+        {
+            _fortune = File.ReadAllText("fortunes.txt", Encoding.UTF8).Split('%');
+        }
+
+        [Command(Description = "Abre una galleta de la suerte")]
+        public async void fortune(Message msg)
+        {
+            await Program.Client.SendTextMessageAsync(msg.Chat, _fortune[_r.Next(_fortune.Length)],
+                replyToMessageId: msg.MessageId);
+        }
+    }
+}
